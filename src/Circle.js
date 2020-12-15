842a1df410f9dc334bac5d9f836676741b33acf2
Kevin Ling <contact@kevinling.xyz>
2020-12-15 15:16:06 +0000

    Abstract components and export ReactNativeDotIntro

---
+++ src/Circle.js
@@ -0,0 +1,171 @@
+import * as React from "react";
+import { TouchableOpacity, Animated, StatusBar, StyleSheet } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
+const AnimatedAntDesign = Animated.createAnimatedComponent(AntDesign);
+
+const colors = [
+  {
+    initialBgColor: "goldenrod",
+    bgColor: "#222",
+    nextBgColor: "#222",
+  },
+  {
+    initialBgColor: "goldenrod",
+    bgColor: "#222",
+    nextBgColor: "yellowgreen",
+  },
+  {
+    initialBgColor: "#222",
+    bgColor: "yellowgreen",
+    nextBgColor: "midnightblue",
+  },
+  {
+    initialBgColor: "yellowgreen",
+    bgColor: "midnightblue",
+    nextBgColor: "turquoise",
+  },
+  {
+    initialBgColor: "midnightblue",
+    bgColor: "turquoise",
+    nextBgColor: "goldenrod",
+  },
+  {
+    initialBgColor: "turquoise",
+    bgColor: "goldenrod",
+    nextBgColor: "#222",
+  },
+];
+
+const Circle = ({ onPress, index, quotes, animatedValue, animatedValue2 }) => {
+  const { initialBgColor, nextBgColor, bgColor } = colors[index];
+  const inputRange = [0, 0.001, 0.5, 0.501, 1];
+  const backgroundColor = animatedValue2.interpolate({
+    inputRange,
+    outputRange: [
+      initialBgColor,
+      initialBgColor,
+      initialBgColor,
+      bgColor,
+      bgColor,
+    ],
+  });
+  const dotBgColor = animatedValue2.interpolate({
+    inputRange: [0, 0.001, 0.5, 0.501, 0.9, 1],
+    outputRange: [
+      bgColor,
+      bgColor,
+      bgColor,
+      initialBgColor,
+      initialBgColor,
+      nextBgColor,
+    ],
+  });
+
+  return (
+    <Animated.View
+      style={[
+        StyleSheet.absoluteFillObject,
+        styles.container,
+        { backgroundColor },
+      ]}
+    >
+      <Animated.View
+        style={[
+          styles.circle,
+          {
+            backgroundColor: dotBgColor,
+            transform: [
+              { perspective: 200 },
+              {
+                rotateY: animatedValue2.interpolate({
+                  inputRange: [0, 0.5, 1],
+                  outputRange: ["0deg", "-90deg", "-180deg"],
+                }),
+              },
+
+              {
+                scale: animatedValue2.interpolate({
+                  inputRange: [0, 0.5, 1],
+                  outputRange: [1, 6, 1],
+                }),
+              },
+
+              {
+                translateX: animatedValue2.interpolate({
+                  inputRange: [0, 0.5, 1],
+                  outputRange: ["0%", "50%", "0%"],
+                }),
+              },
+            ],
+          },
+        ]}
+      >
+        <TouchableOpacity onPress={onPress}>
+          <Animated.View
+            style={[
+              styles.button,
+              {
+                transform: [
+                  {
+                    scale: animatedValue.interpolate({
+                      inputRange: [0, 0.05, 0.5, 1],
+                      outputRange: [1, 0, 0, 1],
+                      // extrapolate: "clamp"
+                    }),
+                  },
+                  {
+                    rotateY: animatedValue.interpolate({
+                      inputRange: [0, 0.5, 0.9, 1],
+                      outputRange: ["0deg", "180deg", "180deg", "180deg"],
+                    }),
+                  },
+                ],
+                opacity: animatedValue.interpolate({
+                  inputRange: [0, 0.05, 0.9, 1],
+                  outputRange: [1, 0, 0, 1],
+                }),
+              },
+            ]}
+          >
+            <AnimatedAntDesign name="arrowright" size={28} color={"white"} />
+          </Animated.View>
+        </TouchableOpacity>
+      </Animated.View>
+    </Animated.View>
+  );
+};
+
+export default Circle;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    paddingTop: StatusBar.currentHeight,
+    padding: 8,
+    paddingBottom: 50,
+  },
+  paragraph: {
+    margin: 12,
+    fontSize: 24,
+    // fontWeight: 'bold',
+    textAlign: "center",
+    fontFamily: "Menlo",
+    color: "white",
+  },
+  button: {
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  circle: {
+    backgroundColor: "turquoise",
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+});
