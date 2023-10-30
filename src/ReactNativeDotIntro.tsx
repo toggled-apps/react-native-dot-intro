adbb189d10d15cf3b62bebb796837eab97bfe8c9
Elvis Lopes <58557455+elvis-s-lopes@users.noreply.github.com>
2023-10-30 16:07:50 -0300

    Update ReactNativeDotIntro.tsx

--- src/ReactNativeDotIntro.tsx
+++ src/ReactNativeDotIntro.tsx
@@ -19,6 +19,7 @@ interface Props {
     bgColor: string;
     nextBgColor: string;
     textColor: string;
+    iconName: string;
   }[];
   contents: { text: string; image: any }[];
   duration?: number;
@@ -29,6 +30,9 @@ interface Props {
   textDuration?: number;
   title: string;
   skip: boolean;
+  onBackward?: () => void;
+  onForward?: () => void;
+  selectedScreenIndex?: number | null;
 }
 
 const ReactNativeDotIntro: React.FC<Props> = (props) => {
@@ -39,7 +43,7 @@ const ReactNativeDotIntro: React.FC<Props> = (props) => {
   const safeContents = props.contents.slice(0, safeColors.length);
 
   const currentColor = safeColors[currentIndex];
-  if (!currentColor) return null; // safety check
+  if (!currentColor) return null;
 
   const mainAnimationValue = React.useRef(new Animated.Value(0)).current;
   const secondaryAnimationValue = React.useRef(new Animated.Value(0)).current;
@@ -51,6 +55,14 @@ const ReactNativeDotIntro: React.FC<Props> = (props) => {
       ? safeContents.map((_, i) => -i * width * 2)
       : [0, -width * 2],
   });
+
+  const handleBackward = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+      sliderAnimationValue.setValue(currentIndex - 1);
+      if (props.onBackward) props.onBackward();
+    }
+  };
 
   const handlePress = () => {
     if (currentIndex + 1 < safeColors.length) {
@@ -74,14 +86,57 @@ const ReactNativeDotIntro: React.FC<Props> = (props) => {
         }),
       ]).start();
       setCurrentIndex((currentIndex + 1) % safeColors.length);
+      if (props.onForward) props.onForward();
     } else {
       props.onEnd();
     }
   };
 
+  // Function to handle screen selection
+  const handleSelectScreen = (index: number) => {
+    if (index >= 0 && index < safeContents.length) {
+      setCurrentIndex(index);
+      sliderAnimationValue.setValue(index);
+      triggerButtonAnimation(); // Trigger the button animation when selecting a specific screen
+    }
+  };
+
+  // Function to trigger the button animation
+  const triggerButtonAnimation = () => {
+    mainAnimationValue.setValue(0);
+    secondaryAnimationValue.setValue(0);
+    Animated.parallel([
+      Animated.timing(mainAnimationValue, {
+        toValue: 1,
+        duration: props.duration || 1000,
+        useNativeDriver: true,
+      }),
+      Animated.timing(secondaryAnimationValue, {
+        toValue: 1,
+        duration: props.duration || 1000,
+        useNativeDriver: false,
+      }),
+    ]).start();
+  };
+
+  React.useEffect(() => {
+    if (props.selectedScreenIndex !== undefined && props.selectedScreenIndex !== null) {
+      // Handle screen selection when the selectedScreenIndex prop changes
+      handleSelectScreen(props.selectedScreenIndex);
+    }
+  }, [props.selectedScreenIndex]);
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar hidden />
+      <View style={styles.navigationButtons}>
+        <Pressable onPress={handleBackward}>
+          <Text style={styles.buttonText}>Back</Text>
+        </Pressable>
+        <Pressable onPress={handlePress}>
+          <Text style={styles.buttonText}>Next</Text>
+        </Pressable>
+      </View>
       <AnimatedCircleButton
         arrowColor={currentColor.arrowColor}
         onPress={handlePress}
@@ -90,6 +145,7 @@ const ReactNativeDotIntro: React.FC<Props> = (props) => {
         initialBgColor={currentColor.initialBgColor}
         nextBgColor={currentColor.nextBgColor}
         bgColor={currentColor.bgColor}
+        iconName={currentColor.iconName}
       />
       <Animated.View
         style={[
@@ -151,6 +207,16 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
     paddingTop: 12,
   },
+  navigationButtons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 24,
+    paddingVertical: 16,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: "#333",
+  },
   header: {
     flexDirection: "row",
     justifyContent: "space-between",
@@ -185,3 +251,4 @@ const styles = StyleSheet.create({
 });
 
 export default ReactNativeDotIntro;
+
